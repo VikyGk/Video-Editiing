a54911e15e6b0f3d0dbcc5f5175a136406daa1bc
VikyGk <53996859+VikyGk@users.noreply.github.com>
2021-05-18 11:58:48 +0530

    Add files via upload

---
+++ lib/routes/upload_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UploadMainpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video Editor'),
+      ),
+      body: Center(
+        child: Text('Testing'),
+      ),
+    );
+  }
+}
